b7cb1f8117fbd3e2f4dd2040ccdb3a3bdf7326d9
BigHeadliner <bigheadliner@gmail.com>
2022-04-19 19:02:27 +0300

    header completely ready

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 $(function(){ 
      
-   $('.header-slider').slick() 
+   $('.header-slider').slick({ 
+       dots:true, 
+       prevArrow:'<button type="button" class="slick-prev"><img src="images/prev.svg" alt="arrow"></button>', 
+       nextArrow:'<button type="button" class="slick-next"><img src="images/next.svg" alt="arrow"></button>', 
+       vertical:true, 
+   }); 
 
 });
