c671798fd64aac96f079abaed3dfeb38789013b2
BigHeadliner <bigheadliner@gmail.com>
2022-05-05 12:50:04 +0300

    start work on product section

--- js/main.js
+++ js/main.js
@@ -1,10 +1,29 @@
 $(function(){ 
      
    $('.header-slider').slick({ 
-       dots:true, 
+       dots: true, 
        prevArrow:'<button type="button" class="slick-prev"><img src="images/prev.svg" alt="arrow"></button>', 
        nextArrow:'<button type="button" class="slick-next"><img src="images/next.svg" alt="arrow"></button>', 
-       vertical:true, 
+       fade: true
    }); 
 
-});
+
+ 
+$('.product__name').slick({
+     slidesToShow: 6,
+     slidesToScroll: 1, 
+     focusOnSelect: true,
+     asNavFor: '.product__content', 
+     vertical: true, 
+     prevArrow:'<button type="button" class="product-prev"><img src="images/product-prev.svg" alt="arrow"></button>', 
+     nextArrow:'<button type="button" class="product-next"><img src="images/product-next.svg" alt="arrow"></button>', 
+  });
+  $('.product__content').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      asNavFor: '.product__name', 
+      fade:true, 
+      arrows:false
+  }); 
+   
+}); 
